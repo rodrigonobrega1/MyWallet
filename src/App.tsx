4692cb148883794ea6331e833aaa5435dade657a
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-06-22 00:17:23 +0100

    create styles css

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Dashboard from "./Pages/Dashboard";
+import GlobalStyles from "./Styles/GlobalStyles";
+
+const App: React.FC = () => {
+
+    return (
+        <>
+        <GlobalStyles />
+        <Dashboard />
+        </>
+    );
+}
+
+export default App;
