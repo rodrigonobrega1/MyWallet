76a591a5b68c8f75dc36317cc935789e8e131889
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-06-23 15:47:58 +0100

    Create and style side bar

--- src/components/Aside/styles.ts
+++ src/components/Aside/styles.ts
@@ -3,6 +3,47 @@ import styled from 'styled-components';
 export const Container = styled.div`
 
     grid-area: AS;
-    color: ${props => props.theme.colors.white};
     background-color: ${props => props.theme.colors.secondary};
+    padding-left: 20px;
+    border-right: 1px solid ${props => props.theme.colors.gray};
+`;
+
+export const Header = styled.header`
+    display: flex;
+    align-items: center;
+    height: 70px;
+`;
+
+export const LogImg = styled.img`
+
+`;
+
+export const Title = styled.h2`
+    color: ${props => props.theme.colors.white};
+    margin-left: 10px;
+`;
+
+export const MenuContainer = styled.nav`
+    display: flex;
+    flex-direction: column;
+    margin-top: 50px;
+`;
+
+export const MenuItemLink = styled.a`
+    color: ${props => props.theme.colors.info};
+    text-decoration: none;
+    margin: 10px 0;
+    display: flex;
+    align-items: center;
+
+    transition: opacity .3s;
+
+    &:hover {
+        opacity: .70;
+    }
+
+    > svg {
+        font-size: 18px;
+        margin-right: 5px;
+    }
 `;
