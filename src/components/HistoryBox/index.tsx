564e82da65335444b51b509500f7b7027afb9828
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-07-18 11:08:13 +0100

    finished line chart

--- src/components/HistoryBox/index.tsx
+++ src/components/HistoryBox/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-import { Container } from './styles';
+import { Container, Header, LegendContainer, Legend } from './styles';
 
 import { ResponsiveContainer, LineChart, Line, XAxis, CartesianGrid, Tooltip} from "recharts";
+
+import formatCurrency from '../../Utils/formatCurrency';
 
 interface IHistoryBoxProps {
     data: {
@@ -20,37 +22,45 @@ const HistoryBox: React.FC<IHistoryBoxProps> = ({
         data, lineColorAmountEntry, lineColorAmountOutput
 }) => (
 
-        <Container>
-            
-            <h2>History Balance</h2>
-
-            <ResponsiveContainer>
-
-                <LineChart data={data} >
-                    <CartesianGrid strokeDasharray="3 3" stroke="#cecece" />
-                    <XAxis dataKey="month" stroke="#cecece"/>
-                    <Tooltip />
-                    <Line
-                        type="monotone"
-                        dataKey="amountEntry"
-                        name="Input"
-                        stroke= {lineColorAmountEntry}
-                        strokeWidth={5}
-                        dot={{r: 5}}
-                        activeDot={{r: 8}}
-                    />
-                    <Line
-                        type="monotone"
-                        dataKey="amountOutput"
-                        name="Output"
-                        stroke= {lineColorAmountOutput}
-                        strokeWidth={5}
-                        dot={{r: 5}}
-                        activeDot={{r: 8}}
-                    />
-                </LineChart>
-            </ResponsiveContainer>
-            
+        <Container> 
+            <Header> 
+               <h2>History Balance</h2>
+               <LegendContainer>
+                        <Legend color={lineColorAmountEntry}>
+                            <div></div>
+                            <span>Input</span>
+                        </Legend>
+                        <Legend color={lineColorAmountOutput}>
+                            <div></div>
+                            <span>Output</span>
+                        </Legend>
+               </LegendContainer>
+            </Header>
+                    <ResponsiveContainer>
+                        <LineChart data={data} >
+                            <CartesianGrid strokeDasharray="3 3" stroke="#cecece" />
+                            <XAxis dataKey="month" stroke="#cecece"/>
+                            <Tooltip formatter={(value: any) => formatCurrency(Number(value))}/>
+                            <Line
+                                type="monotone"
+                                dataKey="amountEntry"
+                                name="Input"
+                                stroke= {lineColorAmountEntry}
+                                strokeWidth={5}
+                                dot={{r: 5}}
+                                activeDot={{r: 8}}
+                            />
+                            <Line
+                                type="monotone"
+                                dataKey="amountOutput"
+                                name="Output"
+                                stroke= {lineColorAmountOutput}
+                                strokeWidth={5}
+                                dot={{r: 5}}
+                                activeDot={{r: 8}}
+                            />
+                        </LineChart>
+                </ResponsiveContainer>
         </Container>
     ) 
 
--- src/components/HistoryBox/styles.ts
+++ src/components/HistoryBox/styles.ts
@@ -1,6 +1,61 @@
 import styled from 'styled-components';
 
+interface ILegendProps {
+    color: string;
+}
+
 export const Container = styled.div`
     width: 100%;
     height: 340px;
+    background-color: ${props => props.theme.colors.secondary};
+    color: ${props => props.theme.colors.white};
+
+    margin: 10px 0;
+    padding: 20px 20px 50px 20px;
+
+    border-radius: 7px;
+
+    
+    `;
+
+export const Header = styled.header`
+
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+
+
+>h2 {
+    padding-left: 3px;
+    margin-bottom: 10px;
+}
+
+`;
+
+export const LegendContainer = styled.ul`
+    list-style: none;
+    display: flex;
+`;
+
+export const Legend = styled.li<ILegendProps>`
+
+    display: flex;
+    align-items: center;
+    margin-bottom: 7px;
+    margin-left: 10px;
+    padding-right: 8px;
+
+    >div {
+        background-color: ${props => props.color};
+        width: 25px;
+        height: 25px;
+        border-radius: 25px;
+        font-size: 14px;
+        line-height: 25px;
+        text-align: center;
+    }
+
+    > span {
+        margin-left: 5px;
+    }
 `;
