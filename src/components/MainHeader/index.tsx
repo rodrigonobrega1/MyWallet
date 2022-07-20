db608bbb3f51e160cbdb1d31c5aa9bb119577cb8
Rodrigo Nobrega <rodrigobool@hotmail.com>
2022-07-20 11:31:06 +0100

    back to code without toggle. doesnt work

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,22 +1,11 @@
-import React, {useMemo, useState} from 'react';
+import React, {useMemo} from 'react';
 
 import emojis from '../../Utils/emojis';
 import Toggle from '../Toggle';
 
-import { useTheme } from '../../hooks/theme';
-
 import { Container, Profile, UserName, Welcome } from './styles';
 
 const MainHeader: React.FC = () => {
-
-    const { toggleTheme, theme} = useTheme();
-
-    const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark' ? true : false);
-
-    const handleChangeTheme = () => {
-        setDarkTheme(!darkTheme);
-        toggleTheme();
-    }
 
     const emoji = useMemo(() => {
         const indice = Math.floor(Math.random() * 8);
@@ -27,12 +16,7 @@ const MainHeader: React.FC = () => {
 
         <Container>
 
-            <Toggle
-                labelLeft='Light'
-                labelRight='Dark'
-                checked={darkTheme}
-                onChange={handleChangeTheme}
-            />
+            <Toggle />
 
             <Profile>
                 <Welcome>
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from './hooks/theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <ThemeProvider>
+
       <App />
-    </ThemeProvider>
+
   </React.StrictMode>
 );
